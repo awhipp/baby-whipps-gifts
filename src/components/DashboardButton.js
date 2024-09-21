730dc92ca403769d7971f2ee22497779be7f5bbe
awhipp <alexruns4@gmail.com>
2024-09-21 14:59:06 -0400

    feat: :sparkles: Adding paypal button to row

--- src/components/DashboardButton.js
+++ src/components/DashboardButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './DashboardButton.css';
 
-const DashboardButton = ({ title, imageUrl, link }) => {
+const DashboardButton = ({ className, title, imageUrl, link }) => {
   return (
-    <a href={link} target="_blank" rel="noopener noreferrer">
+    <a class={className} href={link} target="_blank" rel="noopener noreferrer">
         <div className="dashboard-button" style={{ backgroundImage: `url(${imageUrl})` }}>
         <div className="button-content">
             <p>{title}</p>
