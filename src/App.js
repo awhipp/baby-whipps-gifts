94165cca4b6ccac16bfd006b430243f3a321f080
Alexander Whipp <alexruns4@gmail.com>
2024-09-25 18:34:19 -0400

    feat: :sparkles: Added links and nice imagery

--- src/App.js
+++ src/App.js
@@ -4,28 +4,29 @@ import DashboardButton from './components/DashboardButton';
 import amazonImage from './images/amazon-registry.png';
 import targetImage from './images/target-registry.jpg';
 import paypalImage from './images/paypal-registry.jpg';
+import babyCover from './images/baby-cover.webp';
 
 function App() {
-  return (
-    <div className="app-container">
-      <header className="app-header">
-        <h1>Baby Whipp's Gifts</h1>
-        <p>A registry portal for all things baby Whipp.</p>
-      </header>
-
-      <main className="app-main">
-        <div className="button-grid">
-          <DashboardButton title="Amazon" imageUrl={amazonImage} link="#"/>
-          <DashboardButton title="Target" imageUrl={targetImage} link="#"/>
-          <DashboardButton title="Cash Contribution" imageUrl={paypalImage} link="#"/>
+    return (
+        <div className="app-container">
+            <header className="app-header" style={{ backgroundImage: `url(${babyCover})` }}>
+                <h1>Baby Whipp's Gifts</h1>
+                <p>A registry portal for all things baby Whipp.</p>
+            </header>
+            
+            <main className="app-main">
+                <div className="button-grid">
+                    <DashboardButton title="Amazon" imageUrl={amazonImage} link="https://www.amazon.com/baby-reg/sarah-whipp-alexander-whipp-february-2025-ashburn/3I2HZN10US7JB?br_dsk_hero_yr=&linkCode=ll2&tag=eve-trade-20&linkId=ed33a970b12aabd0e45844c65697c888&language=en_US&ref_=as_li_ss_tl"/>
+                    <DashboardButton title="Target" imageUrl={targetImage} link="https://www.target.com/gift-registry/gift/whipp-baby-2025"/>
+                    <DashboardButton title="Cash Contribution" imageUrl={paypalImage} link="https://www.paypal.com/paypalme/alexanderwhipp"/>
+                </div>
+            </main>
+            
+            <footer className="app-footer">
+                <p>Coming January 2025</p>
+            </footer>
         </div>
-      </main>
-
-      <footer className="app-footer">
-        <p>Coming Early 2025</p>
-      </footer>
-    </div>
-  );
+    );
 }
 
 export default App;
